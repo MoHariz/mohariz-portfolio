9d34633fd38ffd8dcd7a7bce2ab43914d5fd78c3
MoHariz <muhammadhariz206@gmail.com>
2024-02-07 12:23:58 -0800

    matched footer spacings with navbar

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,8 +6,8 @@ import { Link } from 'gatsby';
 
 const Footer: React.FC = () => {
   return (
-    <footer className='w-full flex justify-center items-center lg:px-40 md:px-20 sm:px-10 px-5 py-4' style={{ maxWidth: '1200px', margin: '0 auto'}}>
-      <div className='flex justify-between items-center w-full'>
+    <footer className='w-full lg:px-40 md:px-20 sm:px-10 px-5 py-4'>
+      <div className='flex justify-between items-center w-full' style={{ maxWidth: '1200px', margin: '0 auto'}}>
         <p style={{ margin: 0 }}>© {new Date().getFullYear()} Muhammad Hariz</p>
 
         <div className='flex gap-4'>
