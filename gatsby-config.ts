a32cf9232b5b5760be897324e641c133df11b580
MoHariz <muhammadhariz206@gmail.com>
2024-02-08 21:09:14 -0800

    added robotstxt plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,91 +1,97 @@
 import type { GatsbyConfig } from "gatsby";
 
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+    path: `.env.${process.env.NODE_ENV}`,
+});
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `mohariz.me`,
-    siteUrl: `https://mohariz.me`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: [
-  "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `mohariz.me`,
+        siteUrl: `https://mohariz.me`,
     },
-    __key: "images"
-  },
-  {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      name: "mohariz.me",
-      short_name: "mohariz.me",
-      start_url: "/",
-      background_color: "#ffffff",
-      theme_color: "##ffffff",
-      // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-      // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-      display: "standalone",
-      icon: "src/images/smile.png", // This path is relative to the root of the site.
-      // An optional attribute which provides support for CORS check.
-      // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-      // Any invalid keyword or empty string defaults to `anonymous`
-      crossOrigin: `use-credentials`,
-    },
-  },
-  {
-    resolve: 'gatsby-source-wordpress',
-    options: {
-      url: process.env.GATSBY_WORDPRESS_URL,
-    }
-  },
-  {
-    resolve: `gatsby-plugin-google-gtag`,
-    options: {
-      // You can add multiple tracking ids and a pageview event will be fired for all of them.
-      trackingIds: [
-        process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID, // Google Analytics / GA
-        // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-        // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
-      ],
-      // This object is used for configuration specific to this plugin
-      pluginConfig: {
-        // Puts tracking script in the head instead of the body
-        head: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Defaults to https://www.googletagmanager.com
-        // origin: "YOUR_SELF_HOSTED_ORIGIN",
-        // Delays processing pageview events on route update (in milliseconds)
-        // delayOnRouteUpdate: 0,
-      },
-    },
-  },
-  // {
-  //   resolve: "gatsby-plugin-google-tagmanager",
-  //   options: {
-  //     id: process.env.GATSBY_GOOGLE_TAG_MANAGER_TRACKING_ID,
+    // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+    // If you use VSCode you can also use the GraphQL plugin
+    // Learn more at: https://gatsby.dev/graphql-typegen
+    graphqlTypegen: true,
+    plugins: [
+        "gatsby-plugin-robots-txt",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-postcss",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "mohariz.me",
+                short_name: "mohariz.me",
+                start_url: "/",
+                background_color: "#ffffff",
+                theme_color: "##ffffff",
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: "standalone",
+                icon: "src/images/smile.png", // This path is relative to the root of the site.
+                // An optional attribute which provides support for CORS check.
+                // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+                // Any invalid keyword or empty string defaults to `anonymous`
+                crossOrigin: `use-credentials`,
+            },
+        },
+        {
+            resolve: "gatsby-source-wordpress",
+            options: {
+                url: process.env.GATSBY_WORDPRESS_URL,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-gtag`,
+            options: {
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID, // Google Analytics / GA
+                    // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+                    // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+                ],
+                // This object is used for configuration specific to this plugin
+                pluginConfig: {
+                    // Puts tracking script in the head instead of the body
+                    head: true,
+                    // Setting this parameter is also optional
+                    respectDNT: true,
+                    // Avoids sending pageview hits from custom paths
+                    // exclude: ["/preview/**", "/do-not-track/me/too/"],
+                    // Defaults to https://www.googletagmanager.com
+                    // origin: "YOUR_SELF_HOSTED_ORIGIN",
+                    // Delays processing pageview events on route update (in milliseconds)
+                    // delayOnRouteUpdate: 0,
+                },
+            },
+        },
+        // {
+        //   resolve: "gatsby-plugin-google-tagmanager",
+        //   options: {
+        //     id: process.env.GATSBY_GOOGLE_TAG_MANAGER_TRACKING_ID,
 
-  //     // Include GTM in development.
-  //     // Defaults to false meaning GTM will only be loaded in production.
-  //     includeInDevelopment: false,
+        //     // Include GTM in development.
+        //     // Defaults to false meaning GTM will only be loaded in production.
+        //     includeInDevelopment: false,
 
-  //     // Specify optional GTM environment details.
-  //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-  //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-  //     // dataLayerName: "YOUR_DATA_LAYER_NAME",
-  //   },
-  // },
-]
+        //     // Specify optional GTM environment details.
+        //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        //     // dataLayerName: "YOUR_DATA_LAYER_NAME",
+        //   },
+        // },
+    ],
 };
 
 export default config;
