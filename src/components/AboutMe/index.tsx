a852e336c1f7b32e5d183aee008884fb25a1f5b4
MoHariz <muhammadhariz206@gmail.com>
2024-02-19 10:57:40 -0800

    make contact envelope more niticeable

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -29,11 +29,13 @@ export default function AboutMe({ featuredImage, content }: AboutMeProps) {
                     <Typography className="mb-0" variant="p">
                         <div className="about-me-content" dangerouslySetInnerHTML={{ __html: content }} />
                     </Typography>
-                    <a href="mailto:muhammadhariz206@gmail.com">
-                        <Tooltip label='Contact me'>
-                            <FontAwesomeIcon icon={faEnvelope} className="w-6 h-6" />
-                        </Tooltip>
-                    </a>
+                    <div className="flex justify-center my-4">
+                        <a href="mailto:muhammadhariz206@gmail.com">
+                            <Tooltip label='Contact me'>
+                                <FontAwesomeIcon icon={faEnvelope} className="w-10 h-10 animate-bounce" />
+                            </Tooltip>
+                        </a>
+                    </div>
                 </div>
             </AnimateOnScroll>
         </div>
