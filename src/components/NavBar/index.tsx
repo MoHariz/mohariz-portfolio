08bcded274f618e04ed7a58046274f7fb169cf6b
MoHariz <muhammadhariz206@gmail.com>
2024-03-30 16:34:10 -0700

    added a bit more clean up

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -7,22 +7,33 @@ const NavBar: React.FC = () => {
   const isNotAtPageTop = useNotAtPageTop();
 
   return (
-    <nav className={"flex fixed w-full lg:px-40 md:px-20 sm:px-10 px-5 py-4 inset-x-0 top-0 z-50  duration-200 border-b" + (isNotAtPageTop ? " border-black dark:border-white bg-yellow-300 dark:backdrop-blur dark:bg-inherit" : " border-transparent")}>
-      <div className='flex justify-between items-center w-full' style={{ maxWidth: '1200px', margin: '0 auto' }}>
+    <nav
+      className={
+        'flex fixed w-full lg:px-40 md:px-20 sm:px-10 px-5 py-4 inset-x-0 top-0 z-50  duration-200 border-b' +
+        (isNotAtPageTop
+          ? ' border-black dark:border-white bg-yellow-300 dark:backdrop-blur dark:bg-inherit'
+          : ' border-transparent')
+      }
+    >
+      <div
+        className='flex justify-between items-center w-full'
+        style={{ maxWidth: '1200px', margin: '0 auto' }}
+      >
         <ThemeToggle />
 
-        <ul className="flex gap-4">
-          <li className="navbar-list-item">
-            <Link activeStyle={{ fontWeight: 'bold' }} to="/">Home</Link>
+        <ul className='flex gap-4'>
+          <li className='navbar-list-item'>
+            <Link activeStyle={{ fontWeight: 'bold' }} to='/'>
+              Home
+            </Link>
           </li>
-          {/* <li>
-            <Link to="/about">About</Link>
-          </li> */}
-          <li className="navbar-list-item">
-            <Link activeStyle={{ fontWeight: 'bold' }} to="/projects">Projects</Link>
+          <li className='navbar-list-item'>
+            <Link activeStyle={{ fontWeight: 'bold' }} to='/projects'>
+              Projects
+            </Link>
           </li>
-          <li className="navbar-list-item">
-            <a href="mailto:muhammadhariz206@gmail.com">Contact</a>
+          <li className='navbar-list-item'>
+            <a href='mailto:muhammadhariz206@gmail.com'>Contact</a>
           </li>
         </ul>
       </div>
